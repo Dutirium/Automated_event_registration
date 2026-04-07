ec2608b364af8473079461198b768364a3b25601
RAJAT kANYAL <161469071+Dutirium@users.noreply.github.com>
2026-04-07 15:01:06 +0530

    Remove Razorpay API keys and add placeholders
    Removed sensitive Razorpay API keys and replaced them with placeholders for security.

--- event_registration_website/backend/config/key.php
+++ event_registration_website/backend/config/key.php
@@ -1,4 +1,5 @@
 <?php
-define("RAZORPAY_KEY_ID", "rzp_test_SJGYrqx6Tl7YRY");
-define("RAZORPAY_KEY_SECRET", "Q58CzFRTK853NnLcyoEI753G");
+// your razorpay api key goes here 
+define("", "");
+define("", "");
 ?>
